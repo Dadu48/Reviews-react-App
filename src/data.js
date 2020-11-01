d34db0ef4564ccd6cdb0cad7a9e22b2bc80b9be1
Dadu <shaikzied878@gmail.com>
2020-10-31 19:36:40 -0700

    First commit

---
+++ src/data.js
@@ -0,0 +1,40 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Shaik Dadapeer",
+    job: "web developer",
+    image:
+      "https://lh3.googleusercontent.com/ogw/ADGmqu8v14WJdYvU7byMUPLBwzJjD0vbuTIy5tKUBh9p=s83-c-mo",
+    text:
+      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 2,
+    name: "S.MD.Ghouse",
+    job: "The Boss",
+    image:
+      "https://scontent.fvtz3-1.fna.fbcdn.net/v/t1.0-9/61602368_877453799303611_758161439704743936_n.jpg?_nc_cat=106&ccb=2&_nc_sid=a4a2d7&_nc_ohc=6GvrZ2eFjpgAX_vbyNn&_nc_ht=scontent.fvtz3-1.fna&oh=2cd8bdd6eeb1a870240e6ae8aa3353d1&oe=5FC1742C",
+    text:
+      "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+  },
+  {
+    id: 3,
+    name: "Chidvilash",
+    job: "intern",
+    image:
+      "https://scontent.fblr4-3.fna.fbcdn.net/v/t1.0-9/118893072_785263332273621_4098774270814909783_n.jpg?_nc_cat=106&ccb=2&_nc_sid=09cbfe&_nc_ohc=tTpqPPfUd-kAX8zO47R&_nc_ht=scontent.fblr4-3.fna&oh=6f8178482b2fc708e8c646353145d2b5&oe=5FC1D113",
+    text:
+      "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+  },
+  {
+    id: 4,
+    name: "Arif",
+    job: "Web Designer",
+    image:
+      "https://scontent.fblr4-2.fna.fbcdn.net/v/t1.0-0/s526x395/96763544_1150530501964287_2788169021965467648_n.jpg?_nc_cat=107&ccb=2&_nc_sid=174925&_nc_ohc=2-w7zPz1b30AX_oC9MP&_nc_ht=scontent.fblr4-2.fna&tp=7&oh=cdc5398ac71b65b1cab0626d67d65597&oe=5FC2665D",
+    text:
+      "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+  },
+];
+
+export default reviews;
